37d177e4a0905df36bf2aab1aff103e553d8b157
luokunquan <dh17328494776@hotmail.com>
2026-06-21 20:56:56 +0800

    chore: release v1.0.2

--- build_tmp.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-go build -trimpath -ldflags "-s -w -X main.version=v1.0.2" -o release\ok-windows-amd64.exe .\cmd\ok
-if %errorlevel% equ 0 (
-    echo OK: windows-amd64
-) else (
-    echo FAIL: windows-amd64
-    exit /b 1
-)
